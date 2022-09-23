18e800707701c61ac56277b178c6052c8603b103
jhon <jonathanbarbosa1324@gmail.com>
2022-09-23 15:58:39 -0300

    Adicionar arquivos de projeto.

---
+++ WebCafe/Entyties/Usuarios.cs
@@ -0,0 +1,12 @@
+﻿namespace WebCafe.Entityes
+{
+    public class Usuarios
+    {
+        public string Code { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
+
